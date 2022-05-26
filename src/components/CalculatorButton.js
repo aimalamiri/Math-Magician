7f9e4cc2739153d1017ae375d849c8132434986d
Aimal Amiri <meaimal@hotmail.com>
2022-05-26 19:29:52 +0430

    Change CalculatorButton component from class to functional component

--- src/components/CalculatorButton.js
+++ src/components/CalculatorButton.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class CalculatorButton extends React.PureComponent {
-  render() {
-    const { text, className, handleClick } = this.props;
-    return (
-      <button className={`btn ${className}`} type="button" onClick={handleClick}>
-        {text}
-      </button>
-    );
-  }
-}
+const CalculatorButton = (props) => {
+  const { text, className, handleClick } = props;
+  return (
+    <button className={`btn ${className}`} type="button" onClick={handleClick}>
+      {text}
+    </button>
+  );
+};
 
 CalculatorButton.defaultProps = {
   className: '',
@@ -21,3 +19,5 @@ CalculatorButton.propTypes = {
   className: PropTypes.string,
   handleClick: PropTypes.func.isRequired,
 };
+
+export default CalculatorButton;
