c3c166a95bf31af558cad2d4c0668a7ab0256e77
Aimal Amiri <meaimal@hotmail.com>
2022-06-01 02:26:08 +0430

    Design the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,15 @@
-const Home = () => {};
+const Home = () => (
+  <div className="py-2">
+    <h1>Welcome to Math Magician</h1>
+    <p className="mt-1">We help you to solve your complex math problems using our magical calculator in a blick of an eye.</p>
+    <div className="py-2">
+      <img
+        src="https://www.wellesley.edu/sites/default/files/styles/news_refresh_hero/public/assets/dailyshot/ds_461390782.jpg?itok=jr0Buv1t"
+        className="w-full"
+        alt="math pic"
+      />
+    </div>
+  </div>
+);
 
 export default Home;
