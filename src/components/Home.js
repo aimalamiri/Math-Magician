12539bdb60a4ad11df3aee4371e00293f9710d5e
Aimal Amiri <meaimal@hotmail.com>
2022-06-01 03:02:14 +0430

    Design the Calculator component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 const Home = () => (
   <div className="py-2">
-    <h1>Welcome to Math Magician</h1>
-    <p className="mt-1">We help you to solve your complex math problems using our magical calculator in a blick of an eye.</p>
+    <h1 className="text-xl">Welcome to Math Magician</h1>
+    <p className="mt-1">We help you to solve your complex math problems using our magical caculator.</p>
     <div className="py-2">
       <img
         src="https://www.wellesley.edu/sites/default/files/styles/news_refresh_hero/public/assets/dailyshot/ds_461390782.jpg?itok=jr0Buv1t"
