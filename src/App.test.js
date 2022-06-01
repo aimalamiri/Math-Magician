9256f051988ad88cdaca63081ad90c17f9447ff8
Aimal Amiri <meaimal@hotmail.com>
2022-06-02 01:23:26 +0430

    Add test for App Component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('App renders correctly', () => {
+  const tree = renderer
+    .create(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
